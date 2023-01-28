461f7ad96567340ffec46ce68465857817d846d8
Vedant91827 <patilvedant91827@gmail.com>
2023-01-28 11:20:58 +0530

    Changes

--- controllers/post.js
+++ controllers/post.js
@@ -1,62 +1,79 @@
-import { db } from "../connect.js"
+import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
-import moment from "moment/moment.js";
+import moment from "moment";
 
+export const getPosts = (req, res) => {
+  const userId = req.query.userId;
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
 
-export const getPosts = (req,res) => {
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
-    const userId = req.query.userId
-
-    const token = req.cookies.accessToken;
-    if(!token) return res.status(401).json("Not logged in !")
-
-    //Validate this token cause it expires
-    jwt.verify(token, "secretkey", (err,userInfo)=>{
-        if(err) return res.status(403).json("Token is not valid!")
-
-        //In our user's timeline he we only see his friends posts
+    //In our user's timeline he we only see his friends posts
         //Using Join we fetch users as all well as posts JOIN will find common record , but if we want ours as well use LEFT JOIN
-        const q = userId ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC` 
-        : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
-        LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
-        ORDER BY p.createdAt DESC`;
 
-        // q = userdId : if we are on our profile page we are not gona select all timeline post , we willl only select our posts
+    // console.log(userId);
+
+    const q =
+      userId !== "undefined"
+        ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+        : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
+    ORDER BY p.createdAt DESC`;
+
+    // q = userdId : if we are on our profile page we are not gona select all timeline post , we willl only select our posts
 
     
         // ? = follower user id should be our id stored as cookie named accessToken in our pc i.e userInfo.id by jwt
         // to see our post as well we add one more userId
 
-        const values = userId ? [userId] : [userInfo.id,userInfo.id]
-        
-        db.query(q,values,(err,data)=>{
-            if(err) return res.status(500).json(err);
-            return res.status(200).json(data);
-        })
+    const values =
+      userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
 
-    })
-
-}
+    db.query(q, values, (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    });
+  });
+};
 
 export const addPost = (req, res) => {
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).json("Not logged in!");
-  
-    jwt.verify(token, "secretkey", (err, userInfo) => {
-      if (err) return res.status(403).json("Token is not valid!");
-  
-      const q =
-        "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
-      const values = [
-        req.body.desc,
-        req.body.img,
-        moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
-        userInfo.id,
-      ];
-  
-      db.query(q, [values], (err, data) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json("Post has been created.");
-      });
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created.");
     });
-  };
+  });
+};
+export const deletePost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q =
+      "DELETE FROM posts WHERE `id`=? AND `userId` = ?";
+
+    db.query(q, [req.params.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if(data.affectedRows>0) return res.status(200).json("Post has been deleted.");
+      return res.status(403).json("You can delete only your post")
+    });
+  });
+};
